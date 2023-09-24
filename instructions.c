38605ed89b0aec49a60c47b602aca578b0c91b36
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-24 23:10:06 +0300

    Added _nop function that does nothing

--- instructions.c
+++ instructions.c
@@ -123,23 +123,3 @@ void _pint(stack_t **doubly, unsigned int cline)
 	}
 	printf("%d\n", (*doubly)->n);
 }
-/**
- * _pop - a function that removes the top element of the stack
- * @doubly: the head of the doubly linked list
- * @cline: the line number
- * Return: nothing
- */
-void _pop(stack_t **doubly, unsigned int cline)
-{
-	stack_t *aux;
-
-	if (doubly == NULL || *doubly == NULL)
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", cline);
-		freer();
-		exit(EXIT_FAILURE);
-	}
-	aux = *doubly;
-	*doubly = (*doubly)->next;
-	free(aux);
-}
