c2a6555a98104d2ff7bf6b1056fa3fb7063f7a29
benard Kiplangat <benardkiplangat97@gmail.com>
2023-09-24 22:46:02 +0300

    updating the instructions descriptions and the function structure to make them more organized

--- instructions.c
+++ instructions.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _push - pushes an element to the stack
- * @doubly: head of the linked list
- * @cline: line number
- * Return: no return
+ * _push - a function that pushes an element to stack
+ * @doubly: the pointer to the head ofa doubly linked list
+ * @cline: dthe line number
+ * Return: nothing
  */
+
 void _push(__attribute__((unused)) stack_t **doubly, unsigned int cline)
 {
 	int n, j;
@@ -17,6 +18,7 @@ void _push(__attribute__((unused)) stack_t **doubly, unsigned int cline)
 		freer();
 		exit(EXIT_FAILURE);
 	}
+
 	for (j = 0; vg.arg[j] != '\0'; j++)
 	{
 		if (!isdigit(vg.arg[j]) && vg.arg[j] != '-')
@@ -34,8 +36,35 @@ void _push(__attribute__((unused)) stack_t **doubly, unsigned int cline)
 		add_dnodeint_end(doubly, n);
 }
 /**
- * _pall - prints all values on the stack
- * @doubly: head of the linked list
+ * _swap - a function that swaps the top two elements of the stack
+ * @doubly: the head of the linked list
+ * @cline: the line number
+ * Return: nothing
+ */
+void _swap(stack_t **doubly, unsigned int cline)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+	if (m < 2)
+	{
+		fprintf(stderr, "L%u: Can't swap, stack too short\n", cline);
+		freer();
+		exit(EXIT_FAILURE);
+	}
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	aux->next = (*doubly)->next;
+	aux->prev = *doubly;
+	(*doubly)->next = aux;
+	(*doubly)->prev = NULL;
+}
+/**
+ * _pall - a function that prints all values on the stack
+ * @doubly: the head of the linked list
  * @cline: line numbers
  * Return: no return
  */
@@ -53,10 +82,10 @@ void _pall(stack_t **doubly, unsigned int cline)
 }
 
 /**
- * _pint - prints the value at the top of the stack
- * @doubly: head of the linked list
- * @cline: line number
- * Return: no return
+ * _pint - a function that prints the value at the top of the stack
+ * @doubly: the head of a doubly linked list
+ * @cline: the line number
+ * Return: nothing
  */
 void _pint(stack_t **doubly, unsigned int cline)
 {
@@ -72,10 +101,10 @@ void _pint(stack_t **doubly, unsigned int cline)
 	printf("%d\n", (*doubly)->n);
 }
 /**
- * _pop - removes the top element of the stack
- * @doubly: head of the linked list
- * @cline: line number
- * Return: no return
+ * _pop - a function that removes the top element of the stack
+ * @doubly: the head of the doubly linked list
+ * @cline: the line number
+ * Return: nothing
  */
 void _pop(stack_t **doubly, unsigned int cline)
 {
@@ -90,32 +119,4 @@ void _pop(stack_t **doubly, unsigned int cline)
 	aux = *doubly;
 	*doubly = (*doubly)->next;
 	free(aux);
-}
-
-/**
- * _swap - swaps the top two elements of the stack
- * @doubly: head of the linked list
- * @cline: line number
- * Return: no return
- */
-void _swap(stack_t **doubly, unsigned int cline)
-{
-	int m = 0;
-	stack_t *aux = NULL;
-
-	aux = *doubly;
-	for (; aux != NULL; aux = aux->next, m++)
-		;
-	if (m < 2)
-	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", cline);
-		freer();
-		exit(EXIT_FAILURE);
-	}
-	aux = *doubly;
-	*doubly = (*doubly)->next;
-	aux->next = (*doubly)->next;
-	aux->prev = *doubly;
-	(*doubly)->next = aux;
-	(*doubly)->prev = NULL;
 }
